f5cf28ddff18f3a4d0f3e6b5a80f07770079274d
Jay Faust <jay.faust3@gmail.com>
2023-03-28 22:53:00 -0400

    More implementation for token validation service

--- pkg/services/TokenValidationService.go
+++ pkg/services/TokenValidationService.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"github.com/golang-jwt/jwt"
 	"../models/application/token/TokenPayload.go"
+	"./SigningCertFetchService.go"
 )
 
 func validateToken(encodedToken: string) (TokenPayload, error) {
