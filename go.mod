29edfe497e01a677422f1f1eaaf118d8fbb24582
Jay Faust <jay.faust3@gmail.com>
2023-04-13 22:15:57 -0400

    Installed rabbit package

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.20
 require (
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.8.0 // indirect
 )
--- pkg/services/GetTokenService.go
+++ pkg/services/GetTokenService.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"log"
-
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -14,13 +12,9 @@ func GetToken(encodedToken string) (string, error) {
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, decodedToken)
-	log.Printf("new token generated")
 
 	byteKey := []byte(pemKey)
-	log.Printf("byteKey generated")
 	reEncodedToken, err := token.SignedString(byteKey)
-
-	log.Printf("reEncodedToken: " + reEncodedToken)
 
 	if err != nil {
 		return encodedToken, err
